e075c67949ef28db5baf8000a23e6bafe1588c06
bakunin95 <jugle66@hotmail.com>
2014-06-25 10:22:05 -0400

    optimisation extract + tableau

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,21 +10,21 @@ var archvisApp = angular.module('archvisApp', ['ngRoute','archvisControllers']);
 archvisApp.config(['$routeProvider','$locationProvider',
   function($routeProvider, $locationProvider) {
     $routeProvider.
-      when('/graph', {
+      when('/graph/', {
         templateUrl: 'views/partials/graph.html',
         controller: 'graphCtrl'
       }).
-      when('/tableau', {
+      when('/tableau/', {
         templateUrl: 'views/partials/tableau.html',
         controller: 'tableauCtrl'
       }).
       when('/',{
-        redirectTo: '/graph'
+        redirectTo: '/graph/'
       });
      /* otherwise({
         redirectTo: '/graph'
       });*/
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
   }]);
 
 var archvisControllers = angular.module('archvisControllers', []);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 
 
-
+/*
     var myCodeMirror = CodeMirror.fromTextArea(document.getElementById('codeEditor'), {
               lineNumbers: true,
               mode: "css"
@@ -9,14 +9,29 @@ jQuery(function($) {
 
 	var browserHeight = document.documentElement.clientHeight;
 	myCodeMirror.getWrapperElement().style.height = (0.8 * browserHeight)+ 'px';
-	myCodeMirror.refresh();
+	myCodeMirror.refresh();*/
 
 
 
 	$('#mainLayout').height($(window).height()).layout({ applyDefaultStyles: true, east: { size: 10}  });
 
+var sorting = function(a, b) {
+	var x = (a.isFolder() ? "0" : "1") + a.title.toLowerCase(), y = (b.isFolder() ? "0" : "1") + b.title.toLowerCase(); 
+	return x === y ? 0 : x > y ? 1 : -1; 
+};
+	var node = $("#tree").fancytree({
+	  source: {
+	    url: "tree/",
+	    cache: false
+	  },
+	});
+
+	$("#tree").fancytree("getRoot").sortChildren(sorting,true).reload(); 
 
 
+
+
+	
 
 
 
